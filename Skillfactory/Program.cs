6a187ac7c332fac864f1678ee07e5ab440eb26b8
nuyada <vova-isaev-2015@mail.ru>
2024-05-30 18:58:23 +0300

    Задание 3.3.2

--- Skillfactory/Program.cs
+++ Skillfactory/Program.cs
@@ -11,9 +11,13 @@ namespace Skillfactory
         static void Main(string[] args)
         {
             const string myName = "Vova";
-            Console.WriteLine($"{myName}\n\tПривет, мир\n\tМне 27 лет\n\tMy name is\n {myName}");
-            Console.WriteLine("Привет,\n мир");
-            Console.WriteLine("\u0023");
+            int myAge = 19;
+            bool HaveIApet = true;
+            double MyShoeSize = 44.5;
+            //Console.WriteLine($"{myName}\n\tПривет, мир\n\tМне 27 лет\n\tMy name is\n {myName}");
+            //Console.WriteLine("Привет,\n мир");
+            //Console.WriteLine("\u0023");
+            Console.WriteLine($"My name is {myName}\nMy age is {myAge}\nDo I have a pet? {HaveIApet}\nMy shoe size is {MyShoeSize}");
             Console.ReadKey();
         }
     }
